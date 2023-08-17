b80c23b72072cae58cbdc2cc3f632a56eb170bb6
Jacob Betz <euhe@pm.me>
2023-08-16 18:05:40 -0700

    Refactor config to use an init() function and variables struct

--- util/config.go
+++ util/config.go
@@ -5,14 +5,36 @@ import (
 	"github.com/knadh/koanf/providers/file"
 	"github.com/knadh/koanf/v2"
 	"log"
+	"time"
 )
 
-func ReadConfig() (k *koanf.Koanf) {
+type configVars struct {
+	UpdateInterval time.Duration
+	GroupProcesses bool
+	TempScale      string
+	Nvidia         bool
+	//mbps           bool
+}
+
+var Config *configVars
+
+func init() {
+	cfg := readConfig()
+
+	Config = &configVars{
+		UpdateInterval: cfg.Duration("UpdateInterval"),
+		GroupProcesses: cfg.Bool("GroupProcesses"),
+		TempScale:      cfg.String("TempScale"),
+		Nvidia:         cfg.Bool("Nvidia"),
+		//mbps:           cfg.Bool("mbps"),
+	}
+}
+
+func readConfig() (k *koanf.Koanf) {
 	k = koanf.New(".")
 
 	if err := k.Load(file.Provider("config.toml"), toml.Parser()); err != nil {
 		log.Fatalf("Error loading config: %v", err)
 	}
-
 	return k
 }
