2cb99a3514c329ffcf05b22067e03259059ad80b
Jacob Betz <euhe@pm.me>
2023-10-09 20:23:29 -0700

    Remove Logger init from debug.go

--- internal/debug.go
+++ internal/debug.go
@@ -1,42 +1,10 @@
 package internal
 
 import (
-	"log"
-	"log/slog"
-	"os"
 	"runtime"
 	"strconv"
 	"strings"
 )
-
-var Logger *slog.Logger
-
-func init() {
-	opts := &slog.HandlerOptions{
-		Level: slog.LevelDebug,
-		//AddSource:   true,
-		//ReplaceAttr: nil,
-	}
-	//todo: handle logging os.Stdout to file
-	var handler slog.Handler = slog.NewTextHandler(os.Stdout, opts)
-
-	if Config.IsProduction {
-		handler = slog.NewJSONHandler(os.Stdout, opts)
-	}
-
-	Logger = slog.New(handler)
-	slog.SetDefault(Logger)
-
-	// log to file
-	f, err := os.OpenFile("debug.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		Logger.Error("Failed to open log file")
-	}
-	defer f.Close()
-	log.SetOutput(f)
-
-	Logger.Info("test")
-}
 
 func GetFuncName() string {
 	_, file, line, _ := runtime.Caller(1)
